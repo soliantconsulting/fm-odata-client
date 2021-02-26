da731d8fd3e504bd72616290b98de458b8510565
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2021-02-26 15:02:13 +0100

    Finish Database unit tests

--- test/database.ts
+++ test/database.ts
@@ -15,10 +15,42 @@ describe('Database', () => {
         database = new Database(connectionStub as unknown as Connection, 'foo');
     });
 
+    describe('batch', () => {
+        it('should call executeBatch on batched connection', async () => {
+            const batchedConnectionStub = sinon.createStubInstance(Connection);
+            connectionStub.batchConnection.returns(batchedConnectionStub as unknown as Connection);
+            await database.batch(() => {});
+            sinon.assert.calledOnce(batchedConnectionStub.executeBatch);
+        });
+
+        it('should call executor with batched database', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            const executor = sinon.fake.returns([]);
+            await database.batch(executor);
+            sinon.assert.calledWith(executor, sinon.match.has('batched', true));
+        });
+
+        it('should return empty array when executor returns void', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            await expect(database.batch(() => {})).to.eventually.be.eql([]);
+        });
+
+        it('should return resolved array promised from executor', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            await expect(database.batch(() => [new Promise(resolve => resolve('foo'))])).to.eventually.be.eql(['foo']);
+        });
+    });
+
     describe('schemaManager', () => {
         it('should return a schema manager instance of the given database', () => {
             const schemaManager = database.schemaManager();
             expect(schemaManager['database']).to.be.equal(database);
+        });
+
+        it('should throw error when called in a batched context', async () => {
+            await expect(database.batch(database => {
+                database.schemaManager();
+            })).to.eventually.be.rejectedWith('Schema alterations are not allowed in a batch operation');
         });
     });
 
@@ -39,6 +71,13 @@ describe('Database', () => {
             expect(tables).to.be.eql([{name: 'baz', kind: 'EntitySet', url: 'https://localhost/fmi/odata/v4/foo/bar'}]);
             sinon.assert.calledWith(connectionStub.fetchJson, '/foo', {});
         });
+
+        it('should throw error when called in a batched context', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            await expect(database.batch(database => {
+                return [database.listTables()];
+            })).to.eventually.be.rejectedWith('Tables cannot be listed in a batch operation');
+        });
     });
 
     describe('getMetadata', () => {
@@ -56,6 +95,13 @@ describe('Database', () => {
                 'Response did not include any table information'
             );
             sinon.assert.calledWith(connectionStub.fetchJson, '/foo/$metadata', {});
+        });
+
+        it('should throw error when called in a batched context', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            await expect(database.batch(database => {
+                return [database.getMetadata()];
+            })).to.eventually.be.rejectedWith('Metadata cannot be retrieved in a batch operation');
         });
     });
 
@@ -78,6 +124,13 @@ describe('Database', () => {
                 method: 'POST',
                 body: '{"scriptParameterValue":"param"}',
             });
+        });
+
+        it('should throw error when called in a batched context', async () => {
+            connectionStub.batchConnection.returns(connectionStub as unknown as Connection);
+            await expect(database.batch(database => {
+                return [database.runScript('foo')];
+            })).to.eventually.be.rejectedWith('Script execution is not allowed in a batch operation');
         });
     });
 
