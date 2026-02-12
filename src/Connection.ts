0e825af41984cb15925e7a9c2b4067974719c17c
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2026-02-12 21:02:41 +0100

    style: run pnpm check

--- src/Connection.ts
+++ src/Connection.ts
@@ -262,9 +262,11 @@ class Connection {
         }
 
         const json = await response.text();
-        const cleanedJson = json.replaceAll("\x00", "").replace(/"(?:(?=(\\?))\1.)*?"/gs, (substring) => {
-            return substring.replace(/(?<!\\)((?:\\\\)*)\n/g, "$1\\n");
-        });
+        const cleanedJson = json
+            .replaceAll("\x00", "")
+            .replace(/"(?:(?=(\\?))\1.)*?"/gs, (substring) => {
+                return substring.replace(/(?<!\\)((?:\\\\)*)\n/g, "$1\\n");
+            });
 
         return (await JSON.parse(cleanedJson)) as T;
     }
